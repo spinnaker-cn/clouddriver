2dad30b9a2b64caf34d896d05c25cb157cb88efa
0027007896 <yi.qilin@iwhalecloud.com>
2026-09-24 16:36:25 +0800

    fix:clouddriver20260924 增加可用区兼容

--- clouddriver-ecloud/src/main/java/com/netflix/spinnaker/clouddriver/ecloud/provider/view/EcloudInstanceTypeProvider.java
+++ clouddriver-ecloud/src/main/java/com/netflix/spinnaker/clouddriver/ecloud/provider/view/EcloudInstanceTypeProvider.java
@@ -9,13 +9,21 @@ import com.netflix.spinnaker.clouddriver.ecloud.cache.Keys;
 import com.netflix.spinnaker.clouddriver.ecloud.model.EcloudInstanceType;
 import com.netflix.spinnaker.clouddriver.model.InstanceTypeProvider;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * @Description: 从缓存查询移动云实例规格并提供给查询接口。
+ * @Author: han.pengfei-ai
+ * @Date: 2024/06/27
+ */
 @Component
+@Slf4j
 public class EcloudInstanceTypeProvider implements InstanceTypeProvider<EcloudInstanceType> {
 
   private final ObjectMapper objectMapper;
@@ -32,6 +40,7 @@ public class EcloudInstanceTypeProvider implements InstanceTypeProvider<EcloudIn
 
   @Override
   public Set<EcloudInstanceType> getAll() {
+    log.info("[ECLOUD-SPEC-DIAG] query start");
     Set<EcloudInstanceType> results = new HashSet<>();
     String globalKey = Keys.getInstanceTypeKey("*", "*", "*", "*");
     Collection<String> allInstanceTypeKeys =
@@ -43,6 +52,28 @@ public class EcloudInstanceTypeProvider implements InstanceTypeProvider<EcloudIn
       Map<String, Object> attributes = allDatum.getAttributes();
       EcloudInstanceType type = objectMapper.convertValue(attributes, EcloudInstanceType.class);
       results.add(type);
+    }
+    if (log.isInfoEnabled()) {
+      Map<String, Integer> countsByZone = new HashMap<>();
+      for (EcloudInstanceType type : results) {
+        if (type == null) {
+          countsByZone.merge("<null>", 1, Integer::sum);
+          continue;
+        }
+        String zoneKey =
+            new StringBuffer()
+                .append(type.getAccount())
+                .append('/')
+                .append(type.getRegion())
+                .append('/')
+                .append(type.getZone())
+                .toString();
+        countsByZone.merge(zoneKey, 1, Integer::sum);
+      }
+      log.info(
+          "[ECLOUD-SPEC-DIAG] query complete, resultCount={}, countsByAccountRegionZone={}",
+          results.size(),
+          countsByZone);
     }
     return results;
   }
